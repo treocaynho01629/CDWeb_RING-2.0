c0fa7531edfa4dbec677f03c6124b185d0e39695
Ha Duc Trong <trumhangrong01629@gmail.com>
2023-05-26 23:07:48 +0700

    -Fixed final

--- front-end/src/hooks/useAuth.jsx
+++ front-end/src/hooks/useAuth.jsx
@@ -1,7 +1,7 @@
 import { useContext, useDebugValue } from "react";
 import AuthContext from "../context/AuthProvider";
 
-const useAuth = () => {
+const useAuth = () => { //Lấy người đang đăng nhập từ Context
     const { auth } = useContext(AuthContext);
     useDebugValue(auth, auth => auth?.user ? "Logged In" : "Logged Out")
     return useContext(AuthContext);
