204bcd17667e6d9edb23e69fdc838ce75aa794ef
Ha Duc Trong <trumhangrong01629@gmail.com>
2023-06-04 05:37:00 +0700

    Fix empty response cases

--- front-end/src/components/Products.jsx
+++ front-end/src/components/Products.jsx
@@ -6,7 +6,7 @@ const Products = ({booksList}) => {
   return (
     <div style={{padding: '20px 0px'}}>
       <Grid container spacing={1}>
-        {booksList.map((book)=>(
+        {booksList?.map((book)=>(
             <Grid key={book.id} item xs={12} sm={4} md={3} lg={2.4}>
               <Product key={book.id} book={book}/>
             </Grid>
