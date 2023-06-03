49bd921abab0e64662c30ff183a0c60bb8f9115a
Ha Duc Trong <trumhangrong01629@gmail.com>
2023-06-04 06:52:29 +0700

    Replace old info

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -232,7 +232,7 @@ const Footer = () => {
         </Wrapper>
         <BotFooter>
             <BotText>Giấy chứng nhận Đăng ý kính doanh do NLU - TP.HCM cấp ngày 01/01/2022.</BotText>
-            <Group>Nhóm 19130248 - 19130018</Group>
+            <Group>DoraZ</Group>
         </BotFooter>
     </Container>
   )
