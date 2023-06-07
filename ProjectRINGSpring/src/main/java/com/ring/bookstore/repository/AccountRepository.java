408a5072389112da66083313c8cf9f32a9f72fcc
Ha Duc Trong <trumhangrong01629@gmail.com>
2023-06-07 08:00:47 +0700

    Fixed some minor jpa bug

--- ProjectRINGSpring/src/main/java/com/ring/bookstore/repository/AccountRepository.java
+++ ProjectRINGSpring/src/main/java/com/ring/bookstore/repository/AccountRepository.java
@@ -31,19 +31,19 @@ public interface AccountRepository extends JpaRepository<Account, Integer>{
 	@Query(value ="""
 	select t.user_name as name, coalesce(t.rv, 0) as data, coalesce(t2.od, 0) as otherData
 	from
-	(select top(7) a.user_name, r.user_id, count(r.id) as rv
+	(SELECT a.user_name, r.user_id, count(r.id) as rv
 	from review r join account a on a.id = r.user_id
-	group by a.user_name, r.user_id order by rv desc) t
+	group by a.user_name, r.user_id order by rv desc limit 7) t
 	left join
-	(select top(7) o2.user_id, sum(o.amount) as od
+	(select o2.user_id, sum(o.amount) as od
 	from order_receipt o2 join order_detail o on o2.id = o.order_id
-	group by o2.user_id order by od desc) t2 on t.user_id = t2.user_id
-	order by otherData desc
+	group by o2.user_id order by od DESC limit 7) t2 on t.user_id = t2.user_id
+	order BY otherData desc
 	""", nativeQuery = true)
 	List<IChartResponse> getTopUser(); //Tìm Top 7 Người dùng (Đặt hàng nhiều và Đánh giá nhiều)
 	
 	@Query("""
-	select a.userName as name, count(b.id) as data, isnull(sum(o.amount), 0) as otherData 
+	select a.userName as name, count(b.id) as data, coalesce(sum(o.amount), 0) as otherData 
 	from Account a join Book b on a.id = b.user.id 
 	left join OrderDetail o on b.id = o.book.id
 	group by a.userName
