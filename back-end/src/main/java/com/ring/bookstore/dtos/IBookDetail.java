091e76a3b9d0fd527c0c6d72180f21b1e2162d76
treocaynho01629 <trumhangrong01629@gmail.com>
2024-09-02 08:56:57 +0700

    + Fixed inconsistent product's size in sliders + Fixed grid container flex size + Fixed number input showed spin buttons + Fixed layout issues in Login/Sign up pages
    + Added some code splitting
    + Added gzip compression
    
    + Improved UI for detail page
    
    + Removed some redundant files
    + Removed redundants dependencies
    
    #Fix later
    - [ ] Fix books load more merge
    - [ ] Fix sort by/direction in books query

---
+++ back-end/src/main/java/com/ring/bookstore/dtos/IBookDetail.java
@@ -0,0 +1,14 @@
+package com.ring.bookstore.dtos;
+
+//Display book info
+public interface IBookDisplay {
+	Integer getId();
+	String getTitle();
+	String getDescription();
+	String getImage();
+	Double getPrice();
+	Integer getAmount();
+	Integer getRateAmount();
+	Integer getRateTotal();
+	Integer getSold();
+}
