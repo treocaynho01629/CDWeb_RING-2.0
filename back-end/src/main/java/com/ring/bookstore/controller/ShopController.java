673fda5c625e50e159f36244d0ce406139c19554
treocaynho01629 <trumhangrong01629@gmail.com>
2024-09-22 11:26:27 +0700

    + Added Shop functions

---
+++ back-end/src/main/java/com/ring/bookstore/controller/ShopController.java
@@ -0,0 +1,104 @@
+package com.ring.bookstore.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import com.ring.bookstore.config.CurrentAccount;
+import com.ring.bookstore.model.Account;
+import com.ring.bookstore.request.ReviewRequest;
+import com.ring.bookstore.service.ReviewService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    //Get reviews
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getReviews(
+            @RequestParam(value = "bookId", required = false) Long bookId,
+            @RequestParam(value = "userId", required = false) Long userId,
+            @RequestParam(value = "rating", defaultValue = "0") Integer rating,
+            @RequestParam(value = "pSize", defaultValue = "5") Integer pageSize,
+            @RequestParam(value = "pageNo", defaultValue = "0") Integer pageNo,
+            @RequestParam(value = "sortBy", defaultValue = "id") String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "asc") String sortDir) {
+        return new ResponseEntity<>(reviewService.getReviews(bookId, userId, rating, pageNo, pageSize, sortBy, sortDir), HttpStatus.OK);
+    }
+
+    //Get reviews for book's {id}
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getReviewsByBookId(@PathVariable("id") Long bookId,
+                                                @RequestParam(value = "rating", defaultValue = "0") Integer rating,
+                                                @RequestParam(value = "pSize", defaultValue = "5") Integer pageSize,
+                                                @RequestParam(value = "pageNo", defaultValue = "0") Integer pageNo,
+                                                @RequestParam(value = "sortBy", defaultValue = "id") String sortBy,
+                                                @RequestParam(value = "sortDir", defaultValue = "asc") String sortDir) {
+        return new ResponseEntity<>(reviewService.getReviewsByBookId(bookId, rating, pageNo, pageSize, sortBy, sortDir), HttpStatus.OK);
+    }
+
+    //Get current user's reviews
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> getUserReviews(@RequestParam(value = "rating", defaultValue = "0") Integer rating,
+                                            @RequestParam(value = "pSize", defaultValue = "5") Integer pageSize,
+                                            @RequestParam(value = "pageNo", defaultValue = "0") Integer pageNo,
+                                            @RequestParam(value = "sortBy", defaultValue = "id") String sortBy,
+                                            @RequestParam(value = "sortDir", defaultValue = "asc") String sortDir,
+                                            @CurrentAccount Account currUser) {
+        return new ResponseEntity<>(reviewService.getUserReviews(currUser, rating, pageNo, pageSize, sortBy, sortDir), HttpStatus.OK);
+    }
+
+    //Review
+    @PostMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> reviewBook(@PathVariable("id") Long bookId,
+                                        @RequestBody @Valid ReviewRequest request,
+                                        @CurrentAccount Account currUser) {
+        return new ResponseEntity<>(reviewService.review(bookId, request, currUser), HttpStatus.OK);
+    }
+
+    //Update review by id
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> updateBook(@PathVariable("id") Long bookId,
+                                        @Valid @RequestBody ReviewRequest request,
+                                        @CurrentAccount Account currUser) {
+        return new ResponseEntity<>(reviewService.updateReview(bookId, request, currUser), HttpStatus.OK);
+    }
+
+    //Delete review
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteReview(@PathVariable("id") Long bookId, @CurrentAccount Account currUser) {
+        return new ResponseEntity<>(reviewService.deleteReview(bookId, currUser), HttpStatus.OK);
+    }
+
+    //Delete multiples reviews in a lists of {ids}
+    @DeleteMapping("/delete-multiples")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteReviews(@RequestParam(value = "isInverse", defaultValue = "false") Boolean isInverse,
+                                           @RequestParam("ids") List<Long> ids) {
+        reviewService.deleteReviews(ids, isInverse);
+        return new ResponseEntity<>("Reviews deleted successfully!", HttpStatus.OK);
+    }
+
+    //Delete all reviews
+    @DeleteMapping("/delete-all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteAllReviews() {
+        reviewService.deleteAllReviews();
+        return new ResponseEntity<>("All reviews deleted successfully!", HttpStatus.OK);
+    }
+}
---
+++ back-end/src/main/java/com/ring/bookstore/service/impl/CouponServiceImpl.java
@@ -0,0 +1,135 @@
+package com.ring.bookstore.service.impl;
+
+import com.ring.bookstore.dtos.ShopDTO;
+import com.ring.bookstore.dtos.mappers.ShopMapper;
+import com.ring.bookstore.dtos.projections.IShopDetail;
+import com.ring.bookstore.enums.RoleName;
+import com.ring.bookstore.exception.HttpResponseException;
+import com.ring.bookstore.exception.ImageResizerException;
+import com.ring.bookstore.exception.ResourceNotFoundException;
+import com.ring.bookstore.model.*;
+import com.ring.bookstore.repository.ShopRepository;
+import com.ring.bookstore.request.ShopRequest;
+import com.ring.bookstore.service.ImageService;
+import com.ring.bookstore.service.ShopService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ShopServiceImpl implements ShopService {
+
+    private final ShopRepository shopRepo;
+    private final ShopMapper shopMapper;
+    private final ImageService imageService;
+
+    @Override
+    public Page<ShopDTO> getShops(Integer pageNo, Integer pageSize, String sortBy, String sortDir, String keyword, Long ownerId) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sortDir.equals("asc") ? Sort.by(sortBy).ascending() //Pagination
+                : Sort.by(sortBy).descending());
+
+        //Fetch from database
+        Page<IShopDetail> shopsList = shopRepo.findShopByFilter(keyword, ownerId, pageable);
+        Page<ShopDTO> shopDtos = shopsList.map(shopMapper::apply);
+        return shopDtos;
+    }
+
+    @Override
+    public ShopDTO getShopById(Long id) {
+        IShopDetail shop = shopRepo.findShopById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Shop does not exists!"));
+
+        ShopDTO shopDTO = shopMapper.apply(shop); //Map to DTO
+        return shopDTO;
+    }
+
+    //Add shop (SELLER)
+    @Transactional
+    public Shop addShop(ShopRequest request, MultipartFile file, Account user) throws IOException, ImageResizerException {
+        Image image = null;
+
+        //Image upload
+        if (file != null) image = imageService.upload(file);
+
+        //Create new shop
+        var shop = Shop.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .image(image)
+                .owner(user)
+                .build();
+        Shop addedShop = shopRepo.save(shop); //Save to database
+        return addedShop;
+    }
+
+    @Transactional
+    public Shop updateShop(Long id, ShopRequest request, MultipartFile file, Account user) throws IOException, ImageResizerException {
+        //Get original shop
+        Shop shop = shopRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Shop not found"));
+
+        //Check if correct seller or admin
+        if (!isOwenerValid(shop, user)) throw new HttpResponseException(HttpStatus.UNAUTHORIZED, "Invalid role!");
+
+        //Image upload/replace
+        if (file != null) { //Contain new image >> upload/replace
+            imageService.deleteImage(shop.getImage().getId()); //Delete old image
+            Image savedImage = imageService.upload(file); //Upload new image
+            shop.setImage(savedImage); //Set new image
+        }
+
+        shop.setName(request.getName());
+        shop.setDescription(request.getDescription());
+
+        //Update
+        Shop updatedShop = shopRepo.save(shop);
+        return updatedShop;
+    }
+
+    @Override
+    public Shop deleteShop(Long id, Account user) {
+        Shop shop = shopRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Shop not found"));
+        //Check if correct seller or admin
+        if (!isOwenerValid(shop, user)) throw new HttpResponseException(HttpStatus.UNAUTHORIZED, "Invalid role!");
+
+        shopRepo.deleteById(id); //Delete from database
+        return shop;
+    }
+
+    @Override
+    public void deleteShops(List<Long> ids, boolean isInverse) {
+        if (isInverse) {
+            shopRepo.deleteByIdIsNotIn(ids);
+        } else {
+            shopRepo.deleteByIdIsIn(ids);
+        }
+    }
+
+    @Override
+    public void deleteAllShops() {
+        shopRepo.deleteAll();
+    }
+
+    //Check valid role function
+    protected boolean isOwenerValid(Shop shop, Account user) {
+        boolean result = false;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication(); //Get current auth
+        //Check if is admin or valid owner id
+        if (shop.getOwner().getId().equals(user.getId())
+                || (auth != null && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals(RoleName.ROLE_ADMIN.toString())))) {
+            result = true;
+        }
+        return result;
+    }
+}
