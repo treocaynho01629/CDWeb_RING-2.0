0f734c632f019f2124a241a3f0f8c17cfd9429c7
treocaynho01629 <trumhangrong01629@gmail.com>
2024-10-14 08:29:31 +0700

    + Changed Category DTO

--- back-end/src/main/java/com/ring/bookstore/dtos/CategoryDTO.java
+++
@@ -1,8 +0,0 @@
-package com.ring.bookstore.dtos;
-
-//Category with preview image
-public record CategoryDTO(Integer id,
-                          String categoryName,
-                          String image) {
-
-}
