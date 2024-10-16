e7dbe49569c59087591f572d5beb33dfa013e3d6
treocaynho01629 <trumhangrong01629@gmail.com>
2024-10-16 08:55:06 +0700

    + Added Publisher mapper

---
+++ back-end/src/main/java/com/ring/bookstore/dtos/mappers/PublisherMapper.java
@@ -0,0 +1,33 @@
+package com.ring.bookstore.dtos.mappers;
+
+import com.ring.bookstore.dtos.ShopDTO;
+import com.ring.bookstore.dtos.projections.IShopDetail;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.function.Function;
+
+@Service
+public class ShopMapper implements Function<IShopDetail, ShopDTO> {
+	
+    @Override
+    public ShopDTO apply(IShopDetail shop) {
+
+        String fileDownloadUri = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/api/images/")
+                .path(shop.getImage())
+                .toUriString();
+
+        return new ShopDTO(shop.getOwnerUsername()
+				, shop.getOwnerId()
+				, shop.getId()
+        		, shop.getName()
+        		, shop.getDescription()
+        		, fileDownloadUri
+        		, shop.getJoinedDate()
+        		, shop.getTotalReviews()
+                , shop.getTotalProducts()
+        		, shop.getTotalFollowers());
+    }
+}
